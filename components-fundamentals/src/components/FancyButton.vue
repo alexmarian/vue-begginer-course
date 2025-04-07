090f95ff64069b7656c5c0366d52d052948ed072
Alex Marian <alexandru.marian.ext@infocert.it>
2025-04-07 14:29:51 +0300

    Fancy button milestone

---
+++ components-fundamentals/src/components/FancyButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue';
+
+const hover = ref(false);
+</script>
+
+<template>
+  <button @mouseenter="hover=true" @mouseleave="hover=false">
+    <slot name="icon" :hover="hover"></slot>
+    <slot>Click Me</slot>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
