6ec2c1a791f6ed51cb8ae5d1151e4ff7bfb73f51
Alex Marian <alexandrumarian19@gmail.com>
2025-04-17 21:17:20 +0300

    Initial router setup

--- vuejs-masterclass/src/router/index.ts
+++ vuejs-masterclass/src/router/index.ts
@@ -1,8 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },{
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue'),
+
+    },
+    {
+      path: '/projects/:id',
+      name: 'project',
+      component: () => import('@/views/ProjectView.vue'),
+    }
   ],
 })
 
